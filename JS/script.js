2e3f2a11b3874c75cdc0636939cd291e55f52228
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-07-08 21:03:57 -0400

    erasing

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById('canvas');
 const colorPicker = document.querySelector('#color-picker');
 const resetCanvas = document.querySelector('#rest-canvas');
+const body = document.querySelector('body');
 
-
+// ==== Tools
+const pen = document.querySelector('.fa-pen');
+const paintBrush = document.querySelector('.fa-paintbrush');
+const eraser = document.querySelector('.fa-eraser');
 
 //? ==== context
 const ctx = canvas.getContext('2d');
@@ -13,15 +17,33 @@ ctx.strokeStyle = 'black';
 // ===
 let lastX = 0;
 let lastY = 0;
+let selectedTool = 'pencil';
 let isDrawing = false;
+let isErasing = false;
 
 /**
  * Draw to the canvas
  */
 function draw(e) {
-    if (!isDrawing) return;
+    if (selectedTool !== 'pencil' || !isDrawing) return;
 
+    document.body.style.cursor = 'crosshair'
+    
     // console.log(e);
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX; 
+    lastY = e.offsetY;
+}
+
+function erase(e) {
+    //TODO: erase drawing
+    if (selectedTool !== 'eraser' || !isErasing) return;
+    console.log('erasing');
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.strokeStyle = 'rbg(2555, 2555, 255, 1)';
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
@@ -40,13 +62,21 @@ function changePenColor() {
 
 
 //? ==========  Events Listeners
+
+// === Draw
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     lastX = e.offsetX;
     lastY = e.offsetY;
     
-})
+});
+
+// === Erase
+canvas.addEventListener('mousemove', erase);
+canvas.addEventListener('mousedown', () => isErasing = true);
+
+// ===
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
@@ -54,3 +84,11 @@ canvas.addEventListener('mouseout', () => isDrawing = false);
 colorPicker.addEventListener('input', changePenColor);
 colorPicker.addEventListener('change', changePenColor);
 resetCanvas.addEventListener('click', () => ctx.reset());
+body.addEventListener('mouseup', function() {
+    this.style.cursor = '';
+});
+
+// === Tools
+pen.addEventListener('click', () => selectedTool = 'pencil');
+paintBrush.addEventListener('click', () => selectedTool = 'paintbrush');
+eraser.addEventListener('click', () => selectedTool = 'eraser');
